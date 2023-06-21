0d34a37a1af6e974dccf6e3b6248cb66b9acc681
Tegaomare <tegaomare@tegas-imac.mynetworksettings.com>
2023-06-21 15:56:51 -0400

    update options

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ const options = [
   {
     name: "option",
     type: "list",
-    message: "Pick an option",
+    message: "What would you like to do?",
     choices: [
-      "view all departments",
-      "view all roles",
-      "view all employees",
-      "add a department",
-      "add a role",
-      "add an employee",
-      "update an employee role",
+      "View All Departments",
+      "View All Roles",
+      "View All Employees",
+      "Add A Department",
+      "Add A Role",
+      "Add An Employee",
+      "Update An Employee Role",
     ],
   },
 ];
