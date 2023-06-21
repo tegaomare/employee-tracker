4b1b36deb40d69fdb062f8db99a10ea7cf5c7677
Tegaomare <tegaomare@tegas-imac.mynetworksettings.com>
2023-06-21 18:33:42 -0400

    created a new sequelize model for department

--- models/department.js
+++ models/department.js
@@ -1,2 +1,9 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+// Create a new Sequelize model for department
+class department extends Model {}
+
+department.init({
+  department_name: DataTypes.STRING(30),
+});
