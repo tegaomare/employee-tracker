7d0c2e396e476a363467c0abd348573ede982bfd
Tegaomare <tegaomare@tegas-imac.mynetworksettings.com>
2023-06-21 18:59:08 -0400

    role table created

--- models/role.js
+++ models/role.js
@@ -1,2 +1,19 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
+
+class Role extends Model {}
+
+Role.init(
+  {
+    title: DataTypes.STRING(30),
+    salary: DataTypes.DECIMAL,
+    department_id: DataTypes.INTEGER,
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: "Role",
+  }
+);
+module.exports = Role;
