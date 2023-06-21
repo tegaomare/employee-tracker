de738322a5ee734be5b811865839b00c46e43a1c
Tegaomare <tegaomare@tegas-imac.mynetworksettings.com>
2023-06-21 18:48:50 -0400

    created employee table

--- models/employee.js
+++ models/employee.js
@@ -2,3 +2,18 @@ const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
 
 class Employee extends Model {}
+
+Employee.init(
+  {
+    first_name: DataTypes.STRING(30),
+    last_name: DataTypes.STRING(30),
+    role_id: DataTypes.INTEGER,
+    manager_id: DataTypes.INTEGER,
+  },
+  {
+    sequelize,
+    timestamps: false,
+    underscored: true,
+    modelName: "book",
+  }
+);
